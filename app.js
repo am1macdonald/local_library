00ace74e4ef7ed126848eb2a93f45bfca210dd55
Adam <am1macdonald@gmail.com>
2022-10-13 17:51:48 -0400

    ad database methods

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const mongoose = require("mongoose");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+
+// Connect mongoose to database
+
+const mongoDB =
+  "mongodb+srv://adam:aT99fKBMgnqv@nodetuts.7fmjijx.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB Connection error:"));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
